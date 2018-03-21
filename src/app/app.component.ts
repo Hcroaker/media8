ba59516a6938f406e18ac925ce2bb0b250cb81f1
Hcroaker <harrison.croaker@hotmail.com>
2018-03-21 14:51:17 +1000

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,22 +22,19 @@ export class AppComponent {
 
   //Page 3
   adminPage3: boolean;
-  uploadSuccess: boolean;
-  selectedFiles: any;
 
   //Page 4
   adminPage4: boolean;
   networks: any;
   seasons: any;
   categories: any;
-  submission: any;
 
   constructor (db: AngularFirestore, public afAuth: AngularFireAuth){
 
     this.networks = ["Media8", "RandomNetwork", "Joe Rogan Experience", "Hardcore History"]
     this.seasons = ["Season 1", "Season 2", "Season 3", "Season 4", "Season 5", "Season 6"]
     this.categories = ["Arts", "Comedy", "Education", "Games and Hobbies", "Politics", "Health", "Kids and Family", "News", "Spirituality and Religion", "Science and Medicine", "Society and Culture", "Sports and Rec", "Technology", "Business", "Film"]
-    this.submission = []
+
 
     //Check if the user is already logged in
     this.afAuth.authState.subscribe((auth) => {
@@ -53,8 +50,6 @@ export class AppComponent {
             this.adminPage3 = false;
           }
     });
-
-    this.uploadSuccess = false;
 
   }
 
@@ -79,67 +74,26 @@ export class AppComponent {
     this.adminPage3 = true;
   }
 
-<<<<<<< HEAD
-=======
-  detectFiles(event){
-    this.selectedFiles = event.target.files;
-  }
-
-  uploadSingle(){
-    let file = this.selectedFiles.item(0);
-    return(
-      this.NetworkService.uploadNetworkProfilePic(file).then(val=>{
-        return val
-      })
-    )
-
-  }
-
-  submitNetwork(name,bio,fb,twitter,youtube,itunes,spotify){
-    if(name.value && bio.value && this.selectedFiles){
-      this.uploadSingle().then(downloadURL =>{
-        console.log(downloadURL)
-        let network = new Network(name.value,bio.value,downloadURL,fb.value,twitter.value,youtube.value,itunes.value,spotify.value)
-        console.log(network.printNetwork());
-        this.NetworkService.addNetwork(<Network>network.getData()).then((value)=>{
-          console.log(value)
-          this.uploadSuccess = true;
-          this.NetworkService.getNetworks().subscribe(networks =>{
-            console.log(networks)
-          });
-        }, (error) => {
-          alert(error)
-        })
-      })
-
-    }else{
-      alert("You must enter the name, bio and profile picture")
-    }
-
-  }
-
-  goHome(){
-    this.uploadSuccess = false;
-    this.adminPage3 = false;
-    this.adminPage2 = true;
-  }
-
-  addPodcastFromNewNetwork(){
-    this.uploadSuccess = false;
-    this.adminPage3 = false;
-    this.adminPage4 = true;
-  }
-
->>>>>>> ee26212949a01e666c9c309a83e74495d595321f
   openPage4(){
     this.adminPage2 = false;
     this.adminPage4 = true;
   }
 
   submitToExistingNetwork(){
-    console.log(this.networks)
+    console.log(this.existingNetworkData)
   }
 
+  existingNetworkData = {
+    network : 'network',
+    episodenum : 'episodenum',
+    episodetitle:'episodetitle',
+    podcastdesc: 'podcastdesc',
+    podcastnotes: 'podcastnotes',
+    youtube: 'youtube',
+    soundcloud: 'soundcloud',
+    spotify: 'spotify'
+
+}
 
 
 }
